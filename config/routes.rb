a25b585b6817166d679d0be01ff3889c4e5035a9
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-12 15:45:27 +0200

    fonction

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'reviews/rating:integer'
-  get 'reviews/restaurants:resources'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  root 'pages#home'
 end
